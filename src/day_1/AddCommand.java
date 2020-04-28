8c1acb125f93636c03bd2eb63c5fc24a9dec9174
Madina Ruzimat <madina.ruzimat@gmail.com>
2020-04-28 15:40:27 -0400

    add ImTired.java

---
+++ src/day_1/AddCommand.java
@@ -0,0 +1,8 @@
+package day_1;
+
+public class AddCommand {
+    public static void main(String[] arg){
+        //hello. how is everyone doing today?
+        // this is some test to see if i made some changes.
+    }
+}
---
+++ src/day_1/ImTired.java
@@ -0,0 +1,7 @@
+package day_1;
+
+public class ImTired {
+    public static void main(String[] arg){
+
+    }
+}
